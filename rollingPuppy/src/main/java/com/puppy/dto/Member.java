16b9dfa18dd1966053a6331683165149b13fd0e4
parksehun <bbvirus@naver.com>
2014-04-04 16:59:51 +0900

    Modify Member DTO Class (Add field, getter, setter)

--- rollingPuppy/src/main/java/com/puppy/dto/Member.java
+++ rollingPuppy/src/main/java/com/puppy/dto/Member.java
@@ -1,20 +1,69 @@
 package com.puppy.dto;
 
+import java.sql.Date;
 import java.sql.Timestamp;
 
 public class Member {
 	private int id;
 	private String email;
 	private String pw;
+	private String sex;
+	private Date birth;
+	private int point;
+	private String nickname_adjective;
+	private String nickname_noun;
 	private Timestamp last_logged_time;
+	private Timestamp created_time;
+	private Timestamp updated_time;
 	
+	public String getSex() {
+		return sex;
+	}
+	public void setSex(String sex) {
+		this.sex = sex;
+	}
+	public Date getBirth() {
+		return birth;
+	}
+	public void setBirth(Date birth) {
+		this.birth = birth;
+	}
+	public int getPoint() {
+		return point;
+	}
+	public void setPoint(int point) {
+		this.point = point;
+	}
+	public String getNickname_adjective() {
+		return nickname_adjective;
+	}
+	public void setNickname_adjective(String nickname_adjective) {
+		this.nickname_adjective = nickname_adjective;
+	}
+	public String getNickname_noun() {
+		return nickname_noun;
+	}
+	public void setNickname_noun(String nickname_noun) {
+		this.nickname_noun = nickname_noun;
+	}
+	public Timestamp getCreated_time() {
+		return created_time;
+	}
+	public void setCreated_time(Timestamp created_time) {
+		this.created_time = created_time;
+	}
+	public Timestamp getUpdated_time() {
+		return updated_time;
+	}
+	public void setUpdated_time(Timestamp updated_time) {
+		this.updated_time = updated_time;
+	}
 	public String getPw() {
 		return pw;
 	}
 	public void setPw(String pw) {
 		this.pw = pw;
 	}
-	
 	public int getId() {
 		return id;
 	}
@@ -27,8 +76,8 @@ public class Member {
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	public String getLast_logged_time() {
-		return ""+last_logged_time;
+	public Timestamp getLast_logged_time() {
+		return last_logged_time;
 	}
 	public void setLast_logged_time(Timestamp last_logged_time) {
 		this.last_logged_time = last_logged_time;
