c9781540d8985d904973dc7d943b2b0d72d7d3e9
Yoon <lvev9925@naver.com>
2014-04-10 00:56:35 +0900

    #111 Apply logback Framework To Project (Test Complete)

--- rollingPuppy/src/main/java/com/puppy/controller/MainController.java
+++ rollingPuppy/src/main/java/com/puppy/controller/MainController.java
@@ -8,12 +8,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class MainController extends HttpServlet {
+	
+	private static final Logger log = LoggerFactory.getLogger(MainController.class);
+	
 	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-			
-
+		log.info("test");
+		
 		RequestDispatcher view = request.getRequestDispatcher("main.jsp");
 		view.forward(request, response); 
 	}
