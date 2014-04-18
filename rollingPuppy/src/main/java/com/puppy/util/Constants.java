c4444de6b4142ae5eb84d0beeefe30eba9e05aa9
Yoon <lvev9925@naver.com>
2014-04-18 17:54:55 +0900

    #126 Sonar를 통한 코드품질 개선 (Create Constants Class, Refactoring LoginController) Refactoring LoginController (Issue is presented by Sonar)

---
+++ rollingPuppy/src/main/java/com/puppy/util/Constants.java
@@ -0,0 +1,14 @@
+package com.puppy.util;
+
+public class Constants {
+	
+	public static final String SESSION_MEMBER_ID = "member.id";
+	public static final String SESSION_NICKNAME_NOUN = "member.nickname_noun";
+	public static final String SESSION_NICKNAME_ADJECTIVE = "member.nickname_adjective";
+	
+	public static final String COOKIE_LAST_LOGGED_EMAIL = "member.lastLoggedEmail";
+	
+	public static final String POST_KEEP_LOGIN = "keepLogin";
+	public static final String POST_EMAIL = "email"; 
+	public static final String POST_PW = "pw";
+}
