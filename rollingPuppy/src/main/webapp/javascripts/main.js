bf5fde2002f5f457f5b5974c04bbbc543d9c1d67
skyhills13 <skyhills13@gmail.com>
2014-04-27 23:59:18 +0900

    #132 naver API를 사용하는 부분을 객체지향적 코드로 수정 change some parts of the code which use naver API to object oriented code

--- rollingPuppy/src/main/webapp/javascripts/main.js
+++ rollingPuppy/src/main/webapp/javascripts/main.js
@@ -125,156 +125,145 @@ var NavList = function(elNavList) {
  * 소은이가 작성한 네이버맵 API관련 소스코드 시작
  **********************************************************************************************************/
 var naverMapSettings = {
-	//Main Page에서 Map영역에 해당하는  div객체
-	naverMap: null,
-	//지도 중심으로 포커싱할 위치를 저장하는 객체 (LatLng 좌표사용)
-	oCenterPoint: null,
-	//맵옵션을 모두 저장하고 있는 지도의 기본이 되는 객체
-	oMap: null,
-	
-	oIcon: null,
-	oMarkerInfoWindow: null,
-	oLabel: null,
-	
-	//Zoom 조절을 위한 함수
-	zoomChange: function(nZoomLevel) {
-		this.oCenterPoint = this.oMap.getCenter();
+		naverMap: null, //Main Page에서 Map영역에 해당하는  div객체
+	    oCenterPoint: null, //지도 중심으로 포커싱할 위치를 저장하는 객체 (LatLng 좌표사용)
+	    oMap: null, //맵옵션을 모두 저장하고 있는 지도의 기본이 되는 객체
+	    oIcon: null,
+	    oMarkerInfoWindow: null,
+	    oLabel: null,
 
-		//change zoom method
-		this.oMap.setPointLevel(this.oCenterPoint, nZoomLevel);
-	},
-	
-	initialize: function(){
-		this.naverMap = getNode("naver_map");
-		var mapDivWidth = getStyle(this.naverMap, "width");
-		var mapDivHeight = getStyle(this.naverMap, "height");
-		this.oCenterPoint = new nhn.api.map.LatLng(37.5010226, 127.0396037);
-		this.oMapClickWithoutMarker = document.getElementById("mapClicker");
-		
-		nhn.api.map.setDefaultPoint('LatLng'); //지도의 설정 값을 조회하는 메서드나 이벤트가 사용하는 좌표 객체의 디폴트 클래스를 설정
-		
-		this.oMap = new nhn.api.map.Map(this.naverMap, {
-			//지도 중심점의 좌표 설정
-		    point: this.oCenterPoint,
-		    //초기 줌 레벨은 10으로 둔다.
-		    zoom: 10, 
-		    //마우스 휠 동작으로 지도를 확대/축소할지 여부
-		    enableWheelZoom: false,
-		    //겹쳐 있는 마커를 클릭했을 때 겹친 마커 목록 표시 여부
-		    detectCoveredMarker: true,
-		    //마우스로 끌어서 지도를 이동할지 여부
-		    enableDragPan: true,
-		    //더블클릭으로 지도를 확대할지 여부
-		    enableDblClickZoom: false, 
-		    //지도 모드(0 : 일반 지도, 1 : 겹침 지도, 2 : 위성 지도)
-		    mapMode: 0, 
-		    //실시간 교통 활성화 여부
-		    activateTrafficMap: false,
-		    //자전거 지도 활성화 여부
-		    activateBicycleMap: false,
-		    //지도의 최소/최대 축척 레벨
-		    minMaxLevel: [1, 14],
-		    size: new nhn.api.map.Size(mapDivWidth, mapDivHeight) //지도의 크기
-		});
-		
-		//px단위의 size객체.
-		var oSize = new nhn.api.map.Size(28, 37);
-		//offset위치 지정
-		var oOffset = new nhn.api.map.Size(14, 37);
-		this.oIcon = new nhn.api.map.Icon('/images/marker_48.png', oSize, oOffset); //마커 설정 정보
-		this.oMarkerInfoWindow = new nhn.api.map.InfoWindow(); // - 마커를 클릭했을 때 뜨는 창. html코드뿐만 아니라 객체도 삽입 가능
-		
-		// - infowindow 표시 여부 지정
-		//여기서는 true로 바꿔도 아무 변화가 없음
-		this.oMarkerInfoWindow.setVisible(false);
-		this.oMap.addOverlay(this.oMarkerInfoWindow); // - 지도에 추가
-		
-		this.oLabel = new nhn.api.map.MarkerLabel(); // 마커 위에 마우스 포인터를 올리면 나타나는 마커 라벨
-		this.oMap.addOverlay(this.oLabel); // - 마커 라벨 지도에 추가. 기본은 라벨이 보이지 않는 상태로 추가됨.
-		
-		//changeVisible : event. 정보창의 표시여부 변경
-		//changeVisible {visible : Boolean} 요렇게 생김
-		//oMarkerInfoWindow에다가 changeVisible이라는 이벤트를 거는데, 이 이벤트가 걸리면 뭘 하냐면, 
-		this.oMarkerInfoWindow.attach('changeVisible', function(oCustomEvent) {
-		    if (oCustomEvent.visible) { //이벤트의 visible값이 true이면
-		    	this.oLabel.setVisible(false); //라벨(마우스를 마커위에 클릭하지 않은채 올렸을때 나오는 창)은 가림
-		    }
-		}.bind(this));
-		
-		// 원하는 동작을 구현한 이벤트 핸들러를 attach함수로 추가.
-		// void attach( String sEvent, Function eventHandler) 이벤트명,  이벤트 핸들러 함수
-		this.oMap.attach('mouseenter', function(oCustomEvent) { // mouseenter: 해당 객체 위에 마우스 포인터를 올림
-		    var oTarget = oCustomEvent.target; //target : 모든 이벤트에 존재하는 프로퍼티로, 해당 이벤트를 발생시킨 객체를 의미
-		    // 마커위에 마우스 올라간거면
-		    if (oTarget instanceof nhn.api.map.Marker) {
-		        var oMarker = oTarget;
-		        this.oLabel.setVisible(true, oMarker); // - 특정 마커를 지정하여 해당 마커의 title을 보여준다.
-		    }
-		}.bind(this));
-		
-		this.oMap.attach('mouseleave', function(oCustomEvent) { //mouseleave : 마우스 포인터가 해당 객체 위를 벗어남
-		    var oTarget = oCustomEvent.target; //http://developer.naver.com/wiki/pages/JavaScript#section-JavaScript-Nhn.api.map.CustomControl의 public properties 부분 참조
-		    // 마커위에서 마우스 나간거면
-		    if (oTarget instanceof nhn.api.map.Marker) {
-		    	this.oLabel.setVisible(false);
-		    }
-		}.bind(this));
-		
-		//move event가 발생한 후 click이벤트가 발생한다.
-		//drag가 시작할때  mapClicker를 화면상에서 보이지 않게끔 처리한다.
-		this.oMap.attach('dragstart', function(oCustomEvent) {
-			this.oMapClickWithoutMarker.style.top = "-2000px";
-		}.bind(this));
-		
-		this.oMap.attach('click', function(oCustomEvent) {
-		    var oTarget = oCustomEvent.target;
-		    this.oMarkerInfoWindow.setVisible(false);
+	    //Zoom 조절을 위한 함수
+	    changeZoom: function(nZoomLevel) {
+	        this.oCenterPoint = this.oMap.getCenter();
 
-		    // 마커 클릭하면
-		    if (oTarget instanceof nhn.api.map.Marker) {
-		        // 겹침 마커 클릭한거면
-		        if (!oCustomEvent.clickCoveredMarker) {
-		        	//최초에 생성해놓은 클릭 객체메뉴를 가져온다.
-		            var menuTemplate = document.getElementById('controlBox');
+	        //change zoom method
+	        this.oMap.setPointLevel(this.oCenterPoint, nZoomLevel);
+	    },
 
-		            // - InfoWindow 에 들어갈 내용은 setContent 로 자유롭게 넣을 수 있습니다. 외부 css를 이용할 수 있으며, 
-		            // - 외부 css에 선언된 class를 이용하면 해당 class의 스타일을 바로 적용할 수 있습니다.
-		            // - 단, DIV 의 position style 은 absolute 가 되면 안되며, 
-		            // - absolute 의 경우 autoPosition 이 동작하지 않습니다. 
-		            this.oMarkerInfoWindow.setContent(menuTemplate); //여기가 info window의 html코드를 넣는 부분
-		            this.oMarkerInfoWindow.setPoint(oTarget.getPoint());
-		            this.oMarkerInfoWindow.setVisible(true);
-		            this.oMarkerInfoWindow.setPosition({ //지도 상에서 정보창을 표시할 위치를 설정 
-		                right: 0,
-		                top: -19
-		            });
-		            
-		            //TODO getPosition 결과값을 읽어서 적절히 autoPosition(value값)으로 이동시키도록 한다.
-		            //this.oMarkerInfoWindow.autoPosition(); //정보 창의 일부 또는 전체가 지도 밖에 있으면, 정보 창 전체가 보이도록 자동으로 지도를 이동 
-		        }
-		    } else {
-		    	
-		    	//클라이언트에 상대적인 수평, 수직좌표 가져오기
-		    	clientPosX = oCustomEvent.event._event.clientX;
-		    	clientPosY = oCustomEvent.event._event.clientY;
-		    	
-		    	//TODO Bind를 통해서 oMapClickWithoutMarker를 object 변수로 선언해야 한다.
-		    	this.oMapClickWithoutMarker.style.position = "absolute";
-		    	this.oMapClickWithoutMarker.style.left = clientPosX+'px';
-		    	this.oMapClickWithoutMarker.style.top = clientPosY +'px';
-		    	
-		    	//전역으로 정의된 oMapClicker 객체에 이벤트가 시작된 (클릭된) 좌표에 대한 Point객체를 이식.
-		    	oMapClicker.oClickPoint = oCustomEvent.point;
-		    }
-		}.bind(this));
-		
-		//네이버에서 자동으로 생성하는 지도 맵  element의 크기자동조절을 위해 %값으로 변경한다. (naver_map하위에 생긴다)
-		var eNmap = document.getElementsByClassName("nmap")[0];
-		eNmap.setAttribute('style', 'width:100%;height:100%;');
-		
-		//setSize를 이용해서 변경을 하면 화면이 전부 날아가는 현상이 발생함..
-		//this.oMap.setSize(new nhn.api.map.Size(this.mapDivWidth, this.mapDivHeight));
-	}
+	    // 원하는 동작을 구현한 이벤트 핸들러를 attach함수로 추가.
+	    // void attach( String sEvent, Function eventHandler) 이벤트명,  이벤트 핸들러 함수
+	    attachEvents : function(){
+	        this.oMarkerInfoWindow.attach('changeVisible', this.changeVisibleEvent).bind(this); 
+	        this.oMap.attach('mouseenter', this.mouseEnterEvent).bind(this); // mouseenter: 해당 객체 위에 마우스 포인터를 올림
+	        this.oMap.attach('mouseleave', this.mouseLeaveEvent).bind(this); //mouseleave : 마우스 포인터가 해당 객체 위를 벗어남
+	        this.oMap.attach('dragstart',this.dragStartEvent).bind(this);
+	        this.oMap.attach('click',this.clickEvent).bind(this);    
+	    },
+
+	    //changeVisible : event. 정보창의 표시여부 변경
+	    //changeVisible {visible : Boolean} 요렇게 생김
+	    //oMarkerInfoWindow에다가 changeVisible이라는 이벤트를 거는데, 이 이벤트가 걸리면 뭘 하냐면, 
+	    changeVisibleEvent : function(oCustomEvent){
+	        if (oCustomEvent.visible) { //이벤트의 visible값이 true이면
+	            this.oLabel.setVisible(false); //라벨(마우스를 마커위에 클릭하지 않은채 올렸을때 나오는 창)은 가림
+	        }
+	    },
+	    
+	    mouseEnterEvent: function(oCustomEvent) { 
+	        var oTarget = oCustomEvent.target; //target : 모든 이벤트에 존재하는 프로퍼티로, 해당 이벤트를 발생시킨 객체를 의미
+	        // 마커위에 마우스 올라간거면
+	        if (oTarget instanceof nhn.api.map.Marker) {
+	            var oMarker = oTarget;
+	            this.oLabel.setVisible(true, oMarker); // - 특정 마커를 지정하여 해당 마커의 title을 보여준다.
+	        }
+	    },
+	    mouseLeaveEvent : function(oCustomEvent) { 
+	        var oTarget = oCustomEvent.target; //http://developer.naver.com/wiki/pages/JavaScript#section-JavaScript-Nhn.api.map.CustomControl의 public properties 부분 참조
+	        // 마커위에서 마우스 나간거면
+	        if (oTarget instanceof nhn.api.map.Marker) {
+	            this.oLabel.setVisible(false);
+	        }
+	    },
+	    
+	    //move event가 발생한 후 click이벤트가 발생한다.
+	    //drag 시작할 때 mapClickWithoutMarker를 화면상에서 보이지 않게끔 처리한다.
+	    dragStartEvent : function(oCustomEvent){
+	        this.oMapClickWithoutMarker.style.top = "-2000px";
+	    },
+
+	    clickEvent : function(oCustomEvent) {
+	        var oTarget = oCustomEvent.target;
+	        this.oMarkerInfoWindow.setVisible(false);
+	        // 마커 클릭하면
+	        if (oTarget instanceof nhn.api.map.Marker) {
+	            // 겹침 마커 클릭한거면
+	            if (!oCustomEvent.clickCoveredMarker) {
+	                //최초에 생성해놓은 클릭 객체메뉴를 가져온다.
+	                var menuTemplate = document.getElementById('controlBox');
+
+	                // - InfoWindow 에 들어갈 내용은 setContent 로 자유롭게 넣을 수 있습니다. 외부 css를 이용할 수 있으며, 
+	                // - 외부 css에 선언된 class를 이용하면 해당 class의 스타일을 바로 적용할 수 있습니다.
+	                // - 단, DIV 의 position style 은 absolute 가 되면 안되며, 
+	                // - absolute 의 경우 autoPosition 이 동작하지 않습니다. 
+	                oMarkerInfoWindow.setContent(menuTemplate); //여기가 info window의 html코드를 넣는 부분
+	                oMarkerInfoWindow.setPoint(oTarget.getPoint());
+	                oMarkerInfoWindow.setVisible(true);
+	                oMarkerInfoWindow.setPosition({ //지도 상에서 정보창을 표시할 위치를 설정 
+	                    right: 0,
+	                    top: -19
+	                });
+
+	                //TODO getPosition 결과값을 읽어서 적절히 autoPosition(value값)으로 이동시키도록 한다.
+	                //oMarkerInfoWindow.autoPosition(); //정보 창의 일부 또는 전체가 지도 밖에 있으면, 정보 창 전체가 보이도록 자동으로 지도를 이동 
+	            }
+	        } else {
+	            
+	                //클라이언트에 상대적인 수평, 수직좌표 가져오기
+	                clientPosX = oCustomEvent.event._event.clientX;
+	                clientPosY = oCustomEvent.event._event.clientY;
+	                
+	                //TODO Bind를 통해서 oMapClickWithoutMarker를 object 변수로 선언해야 한다.
+	                this.oMapClickWithoutMarker.style.position = "absolute";
+	                this.oMapClickWithoutMarker.style.left = clientPosX+'px';
+	                this.oMapClickWithoutMarker.style.top = clientPosY +'px';
+	                
+	                //전역으로 정의된 oMapClicker 객체에 이벤트가 시작된 (클릭된) 좌표에 대한 Point객체를 이식.
+	                oMapClicker.oClickPoint = oCustomEvent.point;
+	        }
+	    },
+
+	    initialize: function() {
+	        this.naverMap = getNode("naver_map");
+	        var mapDivWidth = getStyle(this.naverMap, "width");
+	        var mapDivHeight = getStyle(this.naverMap, "height");
+	        this.oCenterPoint = new nhn.api.map.LatLng(37.5010226, 127.0396037);
+	        this.oMapClickWithoutMarker = document.getElementById("mapClicker");
+
+	        nhn.api.map.setDefaultPoint('LatLng'); //지도의 설정 값을 조회하는 메서드나 이벤트가 사용하는 좌표 객체의 디폴트 클래스를 설정
+
+	        this.oMap = new nhn.api.map.Map(this.naverMap, {
+	            point: this.oCenterPoint, //지도 중심점의 좌표 설정
+	            zoom: 10, //초기 줌 레벨은 10으로 둔다.
+	            enableWheelZoom: false, //마우스 휠 동작으로 지도를 확대/축소할지 여부
+	            detectCoveredMarker: true, //겹쳐 있는 마커를 클릭했을 때 겹친 마커 목록 표시 여부
+	            enableDragPan: true,             //마우스로 끌어서 지도를 이동할지 여부
+	            enableDblClickZoom: false,             //더블클릭으로 지도를 확대할지 여부
+	            mapMode: 0, //지도 모드(0 : 일반 지도, 1 : 겹침 지도, 2 : 위성 지도)
+	            activateTrafficMap: false, //실시간 교통 활성화 여부
+	            activateBicycleMap: false, //자전거 지도 활성화 여부
+	            minMaxLevel: [1, 14], //지도의 최소/최대 축척 레벨
+	            size: new nhn.api.map.Size(mapDivWidth, mapDivHeight) //지도의 크기
+	        });
+
+	        var oSize = new nhn.api.map.Size(28, 37); //px단위의 size객체.
+	        
+	        var oOffset = new nhn.api.map.Size(14, 37); //offset위치 지정
+	        this.oIcon = new nhn.api.map.Icon('/images/marker_48.png', oSize, oOffset); //마커 설정 정보
+	        this.oMarkerInfoWindow = new nhn.api.map.InfoWindow(); // - 마커를 클릭했을 때 뜨는 창. html코드뿐만 아니라 객체도 삽입 가능
+	        
+	        this.oMarkerInfoWindow.setVisible(false);   // - infowindow 표시 여부 지정
+	                                                    //여기서는 true로 바꿔도 아무 변화가 없음  
+	        this.oMap.addOverlay(this.oMarkerInfoWindow); // - 지도에 추가
+	        this.oLabel = new nhn.api.map.MarkerLabel(); // 마커 위에 마우스 포인터를 올리면 나타나는 마커 라벨
+	        this.oMap.addOverlay(this.oLabel); // - 마커 라벨 지도에 추가. 기본은 라벨이 보이지 않는 상태로 추가됨.
+
+	         //네이버에서 자동으로 생성하는 지도 맵  element의 크기자동조절을 위해 %값으로 변경한다. (naver_map하위에 생긴다)
+	        var eNmap = document.getElementsByClassName("nmap")[0];
+	        eNmap.setAttribute('style', 'width:100%;height:100%;');
+	        
+	        //setSize를 이용해서 변경을 하면 화면이 전부 날아가는 현상이 발생함..
+	        //this.oMap.setSize(new nhn.api.map.Size(this.mapDivWidth, this.mapDivHeight));
+	    }
 };
 
 /*********************************************************************************************************
@@ -490,6 +479,7 @@ function initialize() {
 	//------------------------------------------------------------------------------------//
 	//네이버맵 초기화영역
 	naverMapSettings.initialize();
+	naverMapSettings.attachEvents();
 	//------------------------------------------------------------------------------------//
 	
 	
