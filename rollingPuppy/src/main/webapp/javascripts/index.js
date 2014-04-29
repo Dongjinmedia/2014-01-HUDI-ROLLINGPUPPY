c192a406d0526abbe8e28ba0527df3af7a949331
skyhills13 <skyhills13@gmail.com>
2014-04-29 16:45:34 +0900

    #172 add code for join & login email validation check

--- rollingPuppy/src/main/webapp/javascripts/index.js
+++ rollingPuppy/src/main/webapp/javascripts/index.js
@@ -1,11 +1,14 @@
+
 function initPage() {
 	var COOKIE_EMAIL = "member.lastLoggedEmail";
 	var lastLoggedEmail = getCookieValue(COOKIE_EMAIL);
+//	var emailToBeChecked = null;
 	
 	fillEmail(lastLoggedEmail);
+
+	document.getElementById('join_form').onsubmit = validateJoinEmail;	
+	document.getElementById('login_form').onsubmit = validateLoginEmail;
 	
-	document.getElementById('join_form').onsubmit = validateEmail;	
-	document.getElementById('login_form').onsubmit = validateEmail;
 }
 
 /**
@@ -38,30 +41,39 @@ function fillEmail(email) {
 	inputEmail.value = email;
 }
 
-//input tag의 name 이 email인 곳에 email형식에 맞게 input이 들어왔는지 정규 표현식을 이용해 확인하는 함
-function validateEmail() {
-	
-	var emails = document.getElementsByName("email");
+//email validation check를 하는 함수
+//두 개의 submit 버튼중 어떤 버튼이 눌렸는지 즉, 어느 칸에 입력된 email에 대하여 validation check를 할 것인지 찾는 함수 
+//function checkClickedButtonJoin() {
+//	var emailToBeChecked = document.getElementById("joinEmail").value;​
+//	validateEmail(emailToBeChecked);
+//}
+//
+//function checkClickedButtonLogin() {
+//	var emailToBeChecked = document.getElementById("loginEmail").value;​
+//	validateEmail(emailToBeChecked);
+//}
+
+//input tag의 name 이 email인 곳에 email형식에 맞게 input이 들어왔는지 정규 표현식을 이용해 확인하는 함수 
+function validateJoinEmail() {
+	var newbieEmail = document.getElementById("joinEmail").value;
 	var emailFormat = /^[a-zA-Z0-9\-_]+(\.[a-zA-Z0-9\-_]+)*@[a-z0-9]+(\-[a-z0-9]+)*(\.[a-z0-9]+(\-[a-z0-9]+)*)*\.[a-z]{2,4}$/;
-	for(i = 0 ; i< emails.length ; i++) {
-		if(emails[i].value != "") {
-			if(emailFormat.test(emails[i].value)) {
-				return true;
-			} else {
-				alert (emails[i].value + "\ndoes not fit to email address form"); 
-			}
+	if (emailFormat.test(newbieEmail)) {
+			return true;
+		} else {
+			alert( newbieEmail + " does not fit to email address form " );
+			return false;
 		}
-	}
 }
 
-//	var newbieEmail = document.getElementById("joinEmail").value;
-//	var emailFormat = /^[a-zA-Z0-9\-_]+(\.[a-zA-Z0-9\-_]+)*@[a-z0-9]+(\-[a-z0-9]+)*(\.[a-z0-9]+(\-[a-z0-9]+)*)*\.[a-z]{2,4}$/;
-//	if (emailFormat.test(newbieEmail)) {
-//			return true;
-//		} else {
-//			alert( newbieEmail + " does not fit to email address form " );
-//			return false;
-//		}
-//}
+function validateLoginEmail() {
+	var oldbieEmail = document.getElementById("loginEmail").value;
+	var emailFormat = /^[a-zA-Z0-9\-_]+(\.[a-zA-Z0-9\-_]+)*@[a-z0-9]+(\-[a-z0-9]+)*(\.[a-z0-9]+(\-[a-z0-9]+)*)*\.[a-z]{2,4}$/;
+	if (emailFormat.test(oldbieEmail)) {
+			return true;
+		} else {
+			alert( oldbieEmail + " does not fit to email address form " );
+			return false;
+		}
+}
 
 window.onload = initPage;
