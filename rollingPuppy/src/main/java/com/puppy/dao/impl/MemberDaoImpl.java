40e67b251227a122e33dda408fe6eab8154980d9
Yoon <lvev9925@naver.com>
2014-05-03 23:18:24 +0900

    #183 Add Register Service Query "NOW" (Insert request To database)

--- rollingPuppy/src/main/java/com/puppy/dao/impl/MemberDaoImpl.java
+++ rollingPuppy/src/main/java/com/puppy/dao/impl/MemberDaoImpl.java
@@ -79,7 +79,7 @@ public class MemberDaoImpl extends DAO implements MemberDao{
 			PreparedStatement selectPreparedStatement = ConnectionPool.getInsertPreparedStatement(nicknameQuery);
 			Member tempMember = selectOne(Member.class, selectPreparedStatement);
 			
-			String query = "INSERT INTO tbl_member(email, pw, nickname_adjective, nickname_noun) VALUES (?, ?, ?, ?)";
+			String query = "INSERT INTO tbl_member(email, pw, nickname_adjective, nickname_noun, created_time) VALUES (?, ?, ?, ?, NOW())";
 			insertPreparedStatement = ConnectionPool.getInsertPreparedStatement(query);
 			insertPreparedStatement.setString(1, member.getEmail());
 			insertPreparedStatement.setString(2, member.getPw());
