6712eb159e0037c63fb59115ab8f4c0839703a0a
skyhills13 <skyhills13@gmail.com>
2014-05-05 17:02:55 +0900

    #125 #51 change code on search.jsp and search.js

--- rollingPuppy/src/main/webapp/javascripts/search.js
+++ rollingPuppy/src/main/webapp/javascripts/search.js
@@ -1,32 +1,39 @@
 var searchAPIkeyRealServer = "513cd098517cce82ec819f7862fb362f";
+var dummyAPIkey = "c1b406b32dbbbbeee5f2a36ddc14067f";
 
+var initialize = function() {
+	var eSubmit = document.getElementById("submit");
+	console.log(eSubmit);
+	
+	eSubmit.addEventListener('click', getResultXml, false);
+}
 
-function getResultXml(){
+function getResultXml(e){
+	e.preventDefault();
 	
 	//var query = document.getElementById("search_box").value;
 	//1. key부분에 key넣고,
 	//2. 갈비집 부분에 query넣고, 
-	var queryUrl = "http://openapi.naver.com/search?key=c1b406b32dbbbbeee5f2a36ddc14067f&query=갈비집&target=local&start=1&display=10"
+	var queryUrl = "http://openapi.naver.com/search?key=513cd098517cce82ec819f7862fb362f&query=갈비집&target=local&start=1&display=10"
 	var request = new XMLHttpRequest();
 	
-//	request.open("GET", queryUrl, false);
-//	request.onreadystatechange = function(){
-//		if ( request.readyState === 4 && request.status === 200 ) {
-//			var result = request.responseXML;
-//		}
-//	}
-//	request.setRequestHeader("Content-Type", "text/xml");
-//	request.sent(null);
-//	console.log(result);
-//	return result;
-	
-	var xmlhttp = new XMLHttpRequest();
-	xmlhttp.open("GET", queryUrl, false);
-	xmlhttp.setRequestHeader('Content-Type', 'text/xml');
-	xmlhttp.send("");
-	xmlDoc = xmlhttp.responseXML;
-	readXML();
-	
+	request.open("GET", queryUrl, false);
+	request.onreadystatechange = function(){
+		console.log("request.readyState : ", request.readyState);
+		console.log("request.status : ", request.status);
+		
+		if ( request.readyState === 4 && request.status === 200 ) {
+			var result = request.responseXML;
+			console.log(result);
+		}
+	}
+	request.setRequestHeader("Content-Type", "text/xml");
+	console.log("test");
+	request.setRequestHeader("Access-Control-Allow-Origin", "*");
+	request.send(null);
+	console.log(result);
+	return result;
+
 }
 
-window.onload = getResultXml;
+window.onload = initialize;
--- rollingPuppy/src/main/webapp/search.jsp
+++ rollingPuppy/src/main/webapp/search.jsp
@@ -3,13 +3,14 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
+<script type="text/javascript" src="/javascripts/search.js"></script>
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 <title>Insert title here</title>
 </head>
 <body>
 	
 	<form action="/search" method="get">
-		<input type = "submit" value = "showxml">
+		<input id="submit" type = "submit" value = "showxml">
 	</form>
 	
 </body>
