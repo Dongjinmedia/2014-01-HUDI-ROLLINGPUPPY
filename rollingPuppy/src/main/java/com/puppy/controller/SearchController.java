6bbbed1e692a0041291969f504dd4e93d27d91dc
skyhills13 <skyhills13@gmail.com>
2014-05-05 21:28:36 +0900

    #125 #51 add code to search controller

--- rollingPuppy/src/main/java/com/puppy/controller/SearchController.java
+++ rollingPuppy/src/main/java/com/puppy/controller/SearchController.java
@@ -11,16 +11,22 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.xpath.XPath;
+import javax.xml.xpath.XPathConstants;
+import javax.xml.xpath.XPathExpressionException;
+import javax.xml.xpath.XPathFactory;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 @SuppressWarnings("serial")
 public class SearchController extends HttpServlet{
 	
 	private static final Logger logger = LoggerFactory.getLogger(SearchController.class);
-	private static final String REAL_SERVER_KEY = "513cd098517cce82ec819f7862fb362f"; 
+	//private static final String REAL_SERVER_KEY = "513cd098517cce82ec819f7862fb362f"; 
 	private static final String DUMMY_KEY = "c1b406b32dbbbbeee5f2a36ddc14067f";
 	private static final String REQUEST_URL = "http://openapi.naver.com/search?key="
 			+ DUMMY_KEY
@@ -40,7 +46,7 @@ public class SearchController extends HttpServlet{
 	//꼭 읽어야 할 레퍼런스 
 	//http://ko.wikipedia.org/wiki/XPath
 	//http://viralpatel.net/blogs/java-xml-xpath-tutorial-parse-xml/
-	public static void getDataFromXML(){
+	public void getDataFromXML(){
 		
 		//dom object 해석을 위해 documentBuilder 객체 필요
 		//documentBuilder 객체를 만들기 위한 팩토리 클래스 선언
@@ -51,15 +57,80 @@ public class SearchController extends HttpServlet{
 		//다른 packege의 document와의 차이점검색해볼것
 		Document document = null;
 		
+		try{
+			//DocumentBuilder클래스를 이용하여 xml 문서로부터 dom document 객체를 가져오기 위한 클래스 선언
+			//결과적으로 프로그래머가 xml해석을 위한 document 객체를 얻을 수 있음.
+			DocumentBuilder builder = builderFactory.newDocumentBuilder();
+			URL url = new URL( REQUEST_URL);
+			
+			//url로부터 xml을 읽어들이기 위한 inputStream 클래스 선언
+			InputStream inputStream = url.openStream();
+			document = builder.parse(inputStream);
+		} catch(Exception e){
+			System.err.println(e);
+		}
 		
-		//DocumentBuilder클래스를 이용하여 xml 문서로부터 dom document 객체를 가져오기 위한 클래스 선언
-		//결과적으로 프로그래머가 xml해석을 위한 document 객체를 얻을 수 있음.
-		DocumentBuilder builder = builderFactory.newDocumentBuilder();
-		URL url = new URL( REQUEST_URL);
+		System.out.println(document);
 		
-		//url로부터 xml을 읽어들이기 위한 inputStream 클래스 선언
-		InputStream inputStream = url.openStream();
-		document = builder.parse(inputStream);
+		//XPathFactory를 통해 XPath 객체를 가져옴
+		XPath xPath = XPathFactory.newInstance().newXPath();
+		
+		/*
+		 * 각각의 검색 결과 값을 담고 있는 리스트 
+		 */
+		
+		NodeList itemNodeList = null;
+		try {
+			String expression = "/rss/channel/item";
+			itemNodeList = (NodeList) xPath.compile(expression).evaluate(document, XPathConstants.NODESET);
+		} catch (XPathExpressionException e) {
+			System.err.println(e);
+		}
+		
+		for(int i = 0 ; i< itemNodeList.getLength(); ++i){
+			Node targetNode = itemNodeList.item(i).getFirstChild();
+			System.out.println("가게명 :" + targetNode.getFirstChild().getNodeValue());
+			
+			Node nextNode = targetNode.getNextSibling();
+			
+			if (nextNode != null)
+				if ( nextNode.getFirstChild() != null)
+					System.out.println("링크 : "+nextNode.getFirstChild().getNodeValue());
+			
+			nextNode = nextNode.getNextSibling();
+			if (nextNode != null)
+				if ( nextNode.getFirstChild() != null)
+					System.out.println("카테고리 : "  + nextNode.getFirstChild().getNodeValue());
+			
+			nextNode = nextNode.getNextSibling();
+			if (nextNode != null)
+				if ( nextNode.getFirstChild() != null)
+					System.out.println("설명 : "  + nextNode.getFirstChild().getNodeValue());
+			
+			
+			nextNode = nextNode.getNextSibling();
+			if (nextNode != null)
+				if ( nextNode.getFirstChild() != null)			
+					System.out.println("전화번호 : "  + nextNode.getFirstChild().getNodeValue());
+			
+			nextNode = nextNode.getNextSibling();
+			if (nextNode != null)
+				if ( nextNode.getFirstChild() != null)
+					System.out.println("주소 : "  + nextNode.getFirstChild().getNodeValue());
+			
+			nextNode = nextNode.getNextSibling();
+			if (nextNode != null)
+				if ( nextNode.getFirstChild() != null)
+					System.out.println("카텍좌표계_X : "  + nextNode.getFirstChild().getNodeValue());
+			
+			nextNode = nextNode.getNextSibling();
+			if (nextNode != null)
+				if ( nextNode.getFirstChild() != null)
+					System.out.println("카텍좌표계_Y : "  + nextNode.getFirstChild().getNodeValue());
+			
+			System.out.println("\n=============================================\n\n\n");
+				
+		}
 		
 		
 	}
