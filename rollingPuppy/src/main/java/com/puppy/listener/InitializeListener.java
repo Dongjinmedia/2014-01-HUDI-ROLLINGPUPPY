ac0b737e8641d2eb923f666b5372a3114df16740
Yoon <lvev9925@naver.com>
2014-05-18 16:58:41 +0900

    #114 Read Data From XML, Print Test Complete.

--- rollingPuppy/src/main/java/com/puppy/listener/InitializeListener.java
+++ rollingPuppy/src/main/java/com/puppy/listener/InitializeListener.java
@@ -1,12 +1,16 @@
 package com.puppy.listener;
 
-import java.io.File;
+import java.io.InputStream;
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.puppy.util.XMLReader;
 
 
 public class InitializeListener implements ServletContextListener{
@@ -17,11 +21,23 @@ public class InitializeListener implements ServletContextListener{
 	public void contextInitialized(ServletContextEvent servletContextEvent) {
 		logger.info("into contextInitialized");
 		
-		String xmlPath = servletContextEvent.getServletContext().getInitParameter("requestUrlMappingXml");
-		logger.info(xmlPath);
+		//XML에 정의된 context-param데이터를 가져온다.
+		String xmlPath = servletContextEvent.getServletContext().getInitParameter("UrlToControllerMappingXmlFileName");
 		
 		//참조 : http://stackoverflow.com/questions/13967307/reading-from-src-main-resources-gives-nullpointerexception
-		logger.info("exists : "+this.getClass().getClassLoader().getResource("url.xml"));
+		InputStream urlXmlStream = this.getClass().getClassLoader().getResourceAsStream(xmlPath);
+		
+		//Util 패키지에 생성된 XMLReader 클래스 객체 생성
+		XMLReader xmlReader = new XMLReader(urlXmlStream);
+		
+		String expression = "/mappings/mapping";
+		List<Map<String, Object>> urlMappingList = xmlReader.getListFromXPath(expression);
+		
+		for (Map<String, Object> map : urlMappingList) {
+			logger.info("===============================");
+			logger.info( "URL: "+ map.get("url"));
+			logger.info( "CONTROLLER: "+ map.get("controller"));
+		}
 	}
 
 	@Override
