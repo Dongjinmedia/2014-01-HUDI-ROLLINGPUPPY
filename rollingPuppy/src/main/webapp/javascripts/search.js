5c3373d9b747c2a5b82eee493f8daaf7a33c8877
skyhills13 <skyhills13@gmail.com>
2014-05-23 16:39:31 +0900

    #249 refactoring search.js and change name of util.js to ajax.js

--- rollingPuppy/src/main/webapp/javascripts/search.js
+++ rollingPuppy/src/main/webapp/javascripts/search.js
@@ -16,19 +16,7 @@ function getResultXml(event){
 		var aResult = JSON.parse(request.responseText); //json을 파싱해서 object로 넣는
 		console.log(aResult);
 		if(aResult.length == 0){
-			//template element가져오기
-			var eDefaultTemplate = document.getElementById("template").querySelector(".default");			
-			//aResult를 for문을 돌며 template element를 복사한 변수를 가져와서 데이터 삽입 
-			var eTarget = document.getElementById("pc_search").querySelector("ul");
-			//이미 존재하는 검색 결과가 있다면 지운다.
-			while (eTarget.firstChild) {
-				eTarget.removeChild(eTarget.firstChild);
-			}
-			var eCopiedDefaultTemplate = eDefaultTemplate.cloneNode(true);
-			eCopiedDefaultTemplate.querySelector(".comment").innerText = "검색 결과가 존재하지 않습니다.";
-			//template을 원하는 위치에 삽입
-			eTarget.appendChild(eCopiedDefaultTemplate);
-			
+			oTemplate.showDefaultTemplate("pc_search", ".comment", "검색 결과가 존재하지 않습니다.")
 		} else {
 			
 			//template element가져오기
@@ -60,5 +48,31 @@ function getResultXml(event){
 	oAjax.getObjectFromJsonGetRequest(incompleteUrl, queryKeyword, callback);
 }
 
+oTemplate = {
+		//이전에 넣었던 templates 지우기
+		deletePreviousTemplate : function(eTarget){
+			while(eTarget.firstChild) {
+				eTarget.removeChild(eTarget.firstChild);
+			}
+		},
+		showDefaultTemplate: function(targetPosId, targetContentsSelector, sExplanation){
+			var eDefaultTemplate = document.getElementById("template").querySelector(".default");
+			var eTarget = document.getElementById(targetPosId).querySelector("ul");
 
-window.onload = initialize;
+			this.deletePreviousTemplate(eTarget);
+
+			var eCopiedDefaultTemplate = eDefaultTemplate.cloneNode(true);
+			eCopiedDefaultTemplate.querySelector(targetContentsSelector).innerText = sExplanation;
+
+			eTarget.appendChild(eCopiedDefaultTemplate);
+		},
+		specifyTemplate : function(aResponse, eDefaultTemplate, eTarget, callback){
+			for( var i = 0 ; aResponse.length ; ++i){
+				var eCopiedDefaultTemplate = eDefaultTemplate.cloneNode(ture);
+				callback(aResponse[i], eDefaultTemplate);
+				eTarget.appendChild(eCopiedDefaultTemplate);
+			}
+		}
+};
+
+window.onload = initialize();
